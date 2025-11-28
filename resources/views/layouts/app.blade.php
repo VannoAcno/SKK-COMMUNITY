c93c2b2da047f1894a4cff554d9334d39605b10b
Lorelei <145348413+Shikein-Lorelei@users.noreply.github.com>
2025-11-28 10:28:11 +0700

    p

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Lobaca</title>
+    
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+</head>
+
+<body class="antialiased">
+    @yield('content')
+</body>
+
+</html>
