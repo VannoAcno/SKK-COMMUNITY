35c5c5db45232905b2206185a0f150ae7e1f3937
Stevano Valentino Verant <stevanno06@gmail.com>
2025-12-02 12:58:57 +0700

    tes

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,16 +1,25 @@
-import './bootstrap';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from './components/LandingPage';
+console.log("🚀 app.jsx loaded");
 
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+console.log("📦 Trying to import LandingPage...");
+import LandingPage from './components/LandingPage';
+import Register from './components/pages/Register';
+
+console.log("✅ LandingPage imported, mounting React...");
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        </Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
+console.log("⚛️ ReactDOM.render called");
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,7 @@
 
 @section('content')
     <div id="app"></div>
+
+    <!-- DIAGNOSTIC TEXT -->
+    <p style="color:red; font-size:24px">TEST BLADE LOADED</p>
 @endsection
